f600bcdf80e87854002449f41e3842535780ab9a
baiyouwen <youwen21@yeah.net>
2017-02-23 17:22:14 +0800

    exwechat

--- application/exwechat/controller/Index.php
+++ application/exwechat/controller/Index.php
@@ -1,10 +1,8 @@
 <?php
 namespace app\exwechat\controller;
 
-
-use think\Request;
-use youwen\exwechat\exWechat;
 use youwen\exwechat\exLog;
+use youwen\exwechat\exWechat;
 
 /**
  * 微信交互控制器
@@ -12,10 +10,13 @@ use youwen\exwechat\exLog;
  */
 class index
 {
+
+    private $_data = [];
+
     public function test()
     {
         echo '<pre>';
-        print_r( 'xx' );
+        print_r('xx');
         exit('</pre>');
     }
     /**
@@ -26,79 +27,133 @@ class index
     public function index()
     {
         exLog::log($_GET, 'get');
-        exLog::log($_POST, 'post');
         $exwechat = new exWechat();
         //如果是绑定，那只输出绑定结果
         if (isset($_GET["echostr"])) {
             $redata = $exwechat->authentication();
             exit($redata);
         }
-        $this->reply();
+        $this->_data = $exwechat->initMsg();
+        $this->_requestHandle();
     }
 
-    /**
-     * 显示创建资源表单页.
-     *
-     * @return \think\Response
-     */
-    public function create()
+    public function _requestHandle()
     {
-        //
+        switch ($this->_data['MsgType']) {
+            // 点击菜单与关注
+            case 'event':$this->_event();
+                break;
+            // 文本消息
+            case 'text':$this->_text();
+                break;
+            // 图片消息
+            case 'image':$this->_image();
+                break;
+            // 音频消息
+            case 'voice':$this->_voice();
+                break;
+            // 视频消息
+            case 'video':$this->_video();
+                break;
+            // 链接
+            case 'link':$this->_link();
+                break;
+            // 地理位置
+            case 'location':$this->_location();
+                break;
+            default:
+                $this->_response_text('这个类型微信消息还没开发呢！other');
+        }
     }
 
-    /**
-     * 保存新建的资源
-     *
-     * @param  \think\Request  $request
-     * @return \think\Response
-     */
-    public function save(Request $request)
+    private function _event()
     {
-        //
+        switch ($this->_data['Event']) {
+            // 关注公众号
+            case 'subscribe':
+                // D('Wevent')->subscribe($this->token, $this->_data);
+                $this->_responseHandle('subscribe');
+                break;
+            // 取消关注公众号
+            case 'unsubscribe':
+                D('Wevent')->unsubscribe($this->token, $this->_data);
+                break;
+            // 扫描带参数二维码事件
+            case 'scan':break;
+            // 上报地理位置事件
+            case 'location':break;
+            // 自定义菜单事件
+            case 'CLICK':break;
+            // 模板消息发送成功通知
+            case 'TEMPLATESENDJOBFINISH':break;
+            // 菜单跳转链接
+            case 'VIEW':break;
+            // 扫码推事件的事件推送
+            case 'scancode_push':break;
+            // 扫码推事件且弹出“消息接收中”提示框的事件推送
+            case 'scancode_waitmsg':break;
+            // 弹出系统拍照发图的事件推送
+            case 'pic_sysphoto':break;
+            // 弹出拍照或者相册发图的事件推送
+            case 'pic_photo_or_album':break;
+            // 弹出微信相册发图器的事件推送
+            case 'pic_weixin':break;
+            // 弹出地理位置选择器的事件推送
+            case 'location_select':break;
+            default:
+                $this->response_text('这个类型事件还没开发呢！event ');
+        }
     }
 
-    /**
-     * 显示指定的资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function read($id)
+    private function _text()
     {
-        //
+        // D('Wtext')->save_msg($this->token, $this->_data);
+        $this->_response();
     }
 
-    /**
-     * 显示编辑资源表单页.
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function edit($id)
+    private function _image()
     {
-        //
+
+    }
+    private function _voice()
+    {
+
+    }
+    private function _video()
+    {
+
+    }
+    private function _link()
+    {
+
+    }
+    private function _location()
+    {
+
     }
 
-    /**
-     * 保存更新的资源
-     *
-     * @param  \think\Request  $request
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function update(Request $request, $id)
+    private function _responseHandle($argument='')
     {
-        //
+        switch (gettype($argument)) {
+            case 'string':
+                $this->_handleKeyword();
+                break;
+            case 'array':
+                
+                break;
+            case 'object':
+            case 'resource':
+            default:
+                return $argument;
+                break;
+        }
     }
-
-    /**
-     * 删除指定资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function delete($id)
+    private function _handleKeyword($keyWord='')
     {
-        //
+        $this->_priorityKeyword($keyWord);
+    }
+    private function _priorityKeyword($keyWord='')
+    {
+
     }
 }
