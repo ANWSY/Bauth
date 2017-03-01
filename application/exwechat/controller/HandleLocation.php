c761a0e6ce263df3072c81c0fc3c9ef618ec483a
baiyouwen <youwen21@yeah.net>
2017-03-01 18:08:23 +0800

    exwechat location

--- application/exwechat/controller/HandleLocation.php
+++ application/exwechat/controller/HandleLocation.php
@@ -14,8 +14,13 @@ class HandleLocation extends AbstractHandle
     public function handle($arrayMsg='', $reylyContext='暂未开发此类型消息')
     {
         $this->msg = empty($arrayMsg) ? $this->exRequest->getMsg() : $arrayMsg;
-        
-        $this->response($reylyContext);
+        $text = "上传个人位置\n";
+        $text .= 'Location_X:'.$this->msg['SendLocationInfo']['Location_X']."\n";
+        $text .= 'Location_Y:'.$this->msg['SendLocationInfo']['Location_Y']."\n";
+        $text .= 'Scale:'.$this->msg['SendLocationInfo']['Scale']."\n";
+        $text .= 'Label:'.$this->msg['SendLocationInfo']['Label']."\n";
+        $text .= 'Poiname:'.$this->msg['SendLocationInfo']['Poiname'];
+        $this->response($text);
 
         exit; //阻止DEBUG信息输出
     }
