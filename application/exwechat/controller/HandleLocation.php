5feb41da6c824fd1287720c4eee4b6f628cd7c38
baiyouwen <youwen21@yeah.net>
2017-03-01 18:05:06 +0800

    添加location

---
+++ application/exwechat/controller/HandleLocation.php
@@ -0,0 +1,22 @@
+<?php
+namespace app\exwechat\controller;
+
+use youwen\exwechat\exLog;
+
+/**
+ * 微信事件消息－控制器
+ *
+ */
+class HandleLocation extends AbstractHandle
+{
+
+    private $msg;
+    public function handle($arrayMsg='', $reylyContext='暂未开发此类型消息')
+    {
+        $this->msg = empty($arrayMsg) ? $this->exRequest->getMsg() : $arrayMsg;
+        
+        $this->response($reylyContext);
+
+        exit; //阻止DEBUG信息输出
+    }
+}
