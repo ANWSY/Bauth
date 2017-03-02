3efde213425123ffdc3a0324272e166d6a6c622d
baiyouwen <youwen21@yeah.net>
2017-03-02 18:43:22 +0800

    exwechat 永久素材添加 统计 下载   临时素材添加 下载   群发消息

---
+++ application/exwechat/controller/Demomedia.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace app\exwechat\controller;
+
+use youwen\exwechat\api\media\media;
+use think\Controller;
+/**
+ * 微信交互控制器
+ * @author baiyouwen <youwen21@yeah.net>
+ */
+class Demomedia extends Controller
+{
+    // 获取素材列表
+    public function batchget_material()
+    {
+        $class = new media($_GET['token']);
+        $ret = $class->batchget_material($_GET['type'], $_GET['offset'], $_GET['count']);
+        echo '<pre>';
+        print_r($ret);
+        exit('</pre>');
+    }
+    // 获取各素材总数统计
+    public function get_materialcount()
+    {
+        $class = new media($_GET['token']);
+        $ret = $class->get_materialcount();
+        echo '<pre>';
+        print_r($ret);
+        exit('</pre>');
+    }
+    // 获取永久素材文件
+    public function get_material()
+    {
+        $class = new media($_GET['token']);
+        $ret = $class->get_material($_GET['media_id']);
+        echo '<pre>';
+        print_r($ret);
+        exit('</pre>');
+    }
+    // 上传永久素材
+    public function add_material()
+    {
+        $class = new media($_GET['token']);
+        $ret = $class->add_material($_GET['img']);
+        echo '<pre>';
+        print_r($ret);
+        exit('</pre>');
+    }
+    // 下载临时素材
+    public function tempGet()
+    {
+        $class = new media($_GET['token']);
+        $ret = $class->tempGet($_GET['media_id']);
+        echo '<pre>';
+        print_r($ret);
+        exit('</pre>');
+    }
+    // 上传临时素材
+    public function tempUpload()
+    {
+        $class = new media($_GET['token']);
+        $ret = $class->tempUpload($_GET['img']);
+        echo '<pre>';
+        print_r($ret);
+        exit('</pre>');
+    }
+}
---
+++ application/exwechat/controller/Xxx.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\exwechat\controller;
+
+use youwen\exwechat\api\media\media;
+use think\Controller;
+/**
+ * 微信交互控制器
+ * @author baiyouwen <youwen21@yeah.net>
+ */
+class xxx extends Controller
+{
+    // 上传临时素材
+    public function test()
+    {
+        echo '<pre>';
+        print_r('xx');
+        exit('</pre>');
+    }
+}
