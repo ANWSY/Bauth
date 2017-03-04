57be22f3b87772d4f08ed7f0d6fd6a910a116d06
zhoujianjun <zhoujianjun307@163.com>
2017-03-04 23:18:39 +0800

    exwechat demojssdk

--- application/exwechat/controller/Demojssdk.php
+++ application/exwechat/controller/Demojssdk.php
@@ -12,9 +12,9 @@ class Demojssdk extends Controller
 {
     public function test()
     {
-        echo '<pre>';
-        print_r( $this->request->url(true) );
-        exit('</pre>');
+        // echo '<pre>';
+        // print_r( $this->request->url(true) );
+        // exit('</pre>');
         echo '<pre>';
         print_r( sha1('jsapi_ticket=sM4AOVdWfPE4DxkXGEs8VA7chf1JZ70Hna9eQyhe7nt8mccytSsdO1rByJOCi5Wz5TlRtO2FsLWo8GMRCvcUig&noncestr=123456&timestamp=1488638082&url=http://demo.bauth.exwechat.com/index.php?s=/exwechat/Demojssdk/index') );
         exit('</pre>');
@@ -26,11 +26,12 @@ class Demojssdk extends Controller
         $nonceStr = '123456';
         $timestamp = 1488638082;
         $url = 'http://demo.bauth.exwechat.com/index.php?s=/exwechat/Demojssdk/index';
+        // $url = 'http://bauth.com/index.php?s=/exwechat/Demojssdk/test1';
         $str = "jsapi_ticket=$jsapi_ticket&noncestr=$nonceStr&timestamp=$timestamp&url=$url";
         $sign = sha1($str);
         $class = new JSSDK('123');
 
-        $url2 = (string)$this->request->url(true);
+        $url2 = $this->request->url(true);
         $signature = $class->signature($jsapi_ticket, $nonceStr, $timestamp, $url2 );
         
         echo '<pre>';
@@ -51,8 +52,8 @@ class Demojssdk extends Controller
         $token = 'ohU0c5NYqySsnLRclt7-so6F5L6qpksZ1Int5s0sHclh9QVLm5tDnG4oMqHi8XH5YwHjKF5_FeMoijARXF1rvrQVeDxDcZyICRq1CpM7MISz7jHF-Ig3cl8uMUatGPEAXJMcABAPBE';
         $class = new JSSDK($token);
         $appId = 'wx70fe57dfaad1a35f';
-        // $timestamp = time();
-        $timestamp = '1488638082';
+        $timestamp = time();
+        // $timestamp = '1488638082';
         // $nonceStr = $class->createNonceStr();
         $nonceStr = '123456';
         // echo '<pre>';
@@ -61,8 +62,8 @@ class Demojssdk extends Controller
         // exit('</pre>');
         // $jsapi_ticket = $class->get_jsapi_ticket();
         $jsapi_ticket = 'sM4AOVdWfPE4DxkXGEs8VA7chf1JZ70Hna9eQyhe7nt8mccytSsdO1rByJOCi5Wz5TlRtO2FsLWo8GMRCvcUig';
-        // $url = $this->request->url(true);
-        $url = 'http://demo.bauth.exwechat.com/index.php?s=/exwechat/Demojssdk/index';
+        $url = $this->request->url(true);
+        // $url = 'http://demo.bauth.exwechat.com/index.php?s=/exwechat/Demojssdk/index';
         $signature = $class->signature($jsapi_ticket, $nonceStr, $timestamp, $url);
         $jsApiList = $class->jsApiList();
         $this->assign('appId', $appId);
